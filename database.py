fe6f75ab8a1ddede20490095fd768376817137aa
qazi_adnan <qazi.adnan.2k1@gmail.com>
2025-09-03 15:52:48 +0530

    No code changes made in database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Adnan@12345@localhost/todo'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Adnan%4012345@localhost/todo'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
